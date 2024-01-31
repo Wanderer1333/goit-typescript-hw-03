10ff6f389585ab446b9413dfda41d360e5ed6b83
Wanderer1333 <wrestling1333@gmail.com>
2024-01-31 19:55:47 +0100

    wandere1333

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key{
+    private signature: number = Math.random();
+
+    getSignature() : number {
+        return this.signature;
+    }
+};
+
+class Person{
+    constructor(public key: Key) { }
+    
+    getKey(): number{
+        return key.getSignature();
+    }
+}
+
+abstract class House{
+    protected door: boolean= false;
+    protected tenants: Person[] = [];
+
+    constructor(protected key: Key){}
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+        
+    };
+    abstract openDoor(trueKey: number): void;
+};
+
+class MyHouse extends House{
+
+    openDoor(key: number) {
+        if (this.key.getSignature() === key) {
+            this.door = true;
+            return;
+        }
+        this.door = false;
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
